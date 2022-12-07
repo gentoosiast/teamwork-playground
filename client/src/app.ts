5f41c05ae02c94ef2b864c67534fdb5266f1d38f
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-12-08 05:47:16 +0900

    feat: apply styles

--- client/src/app.ts
+++ client/src/app.ts
@@ -2,6 +2,6 @@ import { Control } from "./common/control";
 
 export class App extends Control<'div'> {
   constructor(parent: HTMLElement) {
-    super(parent, 'div', '', '');
+    super(parent, 'div', 'main-container', '');
   }
 }
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,4 @@
 import { App } from "./app";
+import "style.css";
 
 const app = new App(document.body);
