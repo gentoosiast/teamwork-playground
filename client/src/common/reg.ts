744919799ad7364ddf5acbe35048bcb00033554f
Sergey Koksharov <gentoosiastic@gmail.com>
2022-12-08 00:22:57 +0300

    fix: correct button class

--- client/src/common/reg.ts
+++ client/src/common/reg.ts
@@ -1,22 +1,19 @@
-import {Control} from "./control";
-
+import { Control } from "./control";
 
 class Button extends Control<HTMLButtonElement> {
-  constructor (parent: HTMLElement, className: string, onClick?: ()=>void) {
-      super(parent, 'button', className);
-      // this.el.onclick = onClick;
+  constructor(parent: HTMLElement, className: string, onClick?: () => void) {
+    super(parent, "button", className);
+    // this.el.onclick = onClick;
   }
 }
 
-
 class RegForm extends Control<HTMLInputElement> {
-  constructor(parent: HTMLElement, className: string = '') {
-    super(parent, 'input', className);
+  constructor(parent: HTMLElement, className: string = "") {
+    super(parent, "input", className);
 
-    const nameForm = new Control(this.el, 'name');
-    const ageForm = new Control(this.el, 'age');
+    const nameForm = new Control(this.el, "name");
+    const ageForm = new Control(this.el, "age");
 
-    const btn = new Button(this.el, 'btn')
-
+    const btn = new Button(this.el, "btn");
   }
 }
