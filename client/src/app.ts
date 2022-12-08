8d0cd54991fc62c73eb86767b5cfa0c0aacd57c3
Anastasia Sim <ASim4@slb.com>
2022-12-08 22:12:24 +0300

    fix: correct regForm

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,7 +1,11 @@
 import { Control } from "./common/control";
+import { RegForm } from "./common/reg";
 
 export class App extends Control<HTMLDivElement> {
   constructor(parent: HTMLElement) {
     super(parent, 'div', 'main-container', '');
+
+
+    const regForm = new RegForm(this.el);
   }
 }
