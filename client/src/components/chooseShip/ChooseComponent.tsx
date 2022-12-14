95d52d348b6b68eeffc72dea2064b486b210daa9
poli8512 <poli8512@gmail.com>
2022-12-14 12:00:49 +0300

    feat:add ChooseComponent

--- client/src/components/chooseShip/ChooseComponent.tsx
+++ client/src/components/chooseShip/ChooseComponent.tsx
@@ -1,21 +1,40 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
-const ChooseComponent=()=>{
-	const [playerShips,setPlayerShips]=useState({
-		huge:1,
-		big:2,
-		middle:3,
-		small:4
+const ChooseComponent = () => {
+	const [playerShips, setPlayerShips] = useState({
+		huge: 1,
+		big: 2,
+		middle: 3,
+		small: 4
 	})
-	return( <div>
+	const canvasRef = useRef(null)
+	useEffect(() => {
+console.log(canvasRef.current,'R')
+	}, [])
+	return (<div style={{border: '1px solid gray', width: '70px'}}>
+			<h5>Расставьте корабли</h5>
 			{
-				Object.entries(playerShips).map(sh=>{
-					return <div>
-						<img src='./public/assets/ship.png'/>
+				Object.entries(playerShips).map((sh, i) => {
+					return <div style={{
+						width: '70px', borderBottom: '1px solid gray',
+						display: "flex"
+					}}>
+
+						<span>{sh[1]}</span>
+						<span style={{flexGrow: '10', textAlign: 'center'}}>
+							<img src='./public/assets/ship.png'
+									 style={{width: `${50 / (i + 1)}px`}}
+							/>
+						</span>
+
 					</div>
 				})
 			}
-	</div>
+			<canvas ref={canvasRef}/>
+		</div>
 	)
 }
 export default ChooseComponent
+
+
+
