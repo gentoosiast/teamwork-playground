097591e902853ba5b2c9cd329c7a071b4def1c35
poli8512 <poli8512@gmail.com>
2022-12-29 13:30:47 +0300

    feat:add area aroundShip

--- client/src/components/chooseShip/CanvasSection.ts
+++ client/src/components/chooseShip/CanvasSection.ts
@@ -33,6 +33,11 @@ export class CanvasSection extends Control {
 	private clickBinded: () => void;
 	private intersectionData: Set<string>;
 	private isReversed: boolean;
+	private boardMatrixEmptyValue: number;
+	private boardMatrixFullValue: number;
+	private circleSteps: { x: number; y: number }[];
+	private boardMatrixBlockedCell: number;
+	private isCurrentIntersect: boolean = false;
 
 	constructor(parentNode: HTMLElement, ships: Record<string, number>, activeShip?: string) {
 		super(parentNode);
@@ -50,17 +55,23 @@ export class CanvasSection extends Control {
 		this.clickBinded = this.onClick.bind(this)
 		this.shipsOnCanvas = []
 		this.boardMatrix = []
+		this.boardMatrixEmptyValue = 99
+		this.boardMatrixFullValue = 1
+		this.boardMatrixBlockedCell = 5
 		this.intersectionData = new Set()
 		this.ctx = this.canvasSection.node.getContext('2d')
-
+		this.circleSteps = [
+			{x: 0, y: 1}, {x: 1, y: 1}, {x: -1, y: 1},
+			{x: 1, y: 0}, {x: -1, y: 0},
+			{x: 0, y: -1}, {x: 1, y: -1}, {x: -1, y: -1}
+		]
 		this.createEmptyMatrix()
-
 		this.drawScene()
 	}
 
 	clearCells(activeShip: string) {
 		if (!this.prevPosX && !this.prevPosY) return
-		this.fillCells(activeShip, 99)
+		this.fillCells(activeShip, this.boardMatrixEmptyValue)
 	}
 
 	fillCells(activeShip: string, value: number) {
@@ -70,13 +81,10 @@ export class CanvasSection extends Control {
 		}
 		if (cells.length === ShipsSizes[activeShip as keyof typeof ShipsSizes]) {
 			for (let i = 0; i < cells.length; i++) {
+				if (this.boardMatrix[this.prevPosY][this.prevPosX + i] === this.boardMatrixBlockedCell) return
 				this.boardMatrix[this.prevPosY][this.prevPosX + i] = value
 			}
 		}
-	}
-
-	changeCellBoard(x: number, y: number, value: number) {
-		this.boardMatrix[y][x] = value
 	}
 
 	addToIntersection(x: number, y: number, lngth: number, rotate: boolean) {
@@ -93,9 +101,10 @@ export class CanvasSection extends Control {
 	}
 
 	onClick() {
+		if (this.isCurrentIntersect) return
 		this.canvasSection.node.removeEventListener('mousemove', this.moveBinded)
 		this.canvasSection.node.removeEventListener('click', this.clickBinded)
-		document.body.removeEventListener('keyup',this.rotateShip)
+		document.body.removeEventListener('keyup', this.rotateShip)
 		this.createImage('./public/assets/ship.png',
 			this.cellSize * ShipsSizes[this.activeShip as keyof typeof ShipsSizes], this.cellSize,
 			(image) => {
@@ -111,6 +120,7 @@ export class CanvasSection extends Control {
 				this.shipsOnCanvas.push(imageObj)
 				this.addToIntersection(this.prevPosX, this.prevPosY, ShipsSizes[this.activeShip as keyof typeof ShipsSizes], false)
 				this.clearCells(this.activeShip)
+				this.fillShipArea(this.prevPosX, this.prevPosY, ShipsSizes[this.activeShip as keyof typeof ShipsSizes])
 				this.drawScene()
 			})
 
@@ -120,12 +130,12 @@ export class CanvasSection extends Control {
 		const sAr = []
 		for (let i = 0; i < lngth; i++) {
 			!this.isReversed
-				? sAr.push(this.intersectionData.has(`${this.prevPosY}-${this.prevPosX+i}`))
-				: sAr.push(this.intersectionData.has(`${this.prevPosY+i}-${this.prevPosX}`))
+				? sAr.push(this.intersectionData.has(`${this.prevPosY}-${this.prevPosX + i}`))
+				: sAr.push(this.intersectionData.has(`${this.prevPosY + i}-${this.prevPosX}`))
 		}
 		//console.log(sAr)
-		return sAr.some(e=>e)
-	//	return this.intersectionData.has(`${this.prevPosY}-${this.prevPosX}`)
+		return sAr.some(e => e)
+		//	return this.intersectionData.has(`${this.prevPosY}-${this.prevPosX}`)
 	}
 
 	isOnBoard(x: number, y: number) {
@@ -140,11 +150,13 @@ export class CanvasSection extends Control {
 			this.clearCells(this.activeShip)
 			this.prevPosX = xC
 			this.prevPosY = yC
-			const isIntersect=this.isIntersection(this.prevPosX, this.prevPosY, ShipsSizes[this.activeShip as keyof typeof ShipsSizes])
+			const isIntersect = this.isIntersection(this.prevPosX, this.prevPosY, ShipsSizes[this.activeShip as keyof typeof ShipsSizes])
 
 			if (isIntersect) {
+				this.isCurrentIntersect = true
 				this.fillCells(this.activeShip, 2)
 			} else {
+				this.isCurrentIntersect = false
 				this.fillCells(this.activeShip, 1)
 				this.canvasSection.node.addEventListener('click', this.clickBinded)
 			}
@@ -153,11 +165,11 @@ export class CanvasSection extends Control {
 		}
 	}
 
-	rotateShip(e:KeyboardEvent) {
+	rotateShip(e: KeyboardEvent) {
 		const keyName = e.key;
 		console.log(e.key, e.code, e.keyCode)
 		if (keyName === 'Space') {
-			this.isReversed=!this.isReversed
+			this.isReversed = !this.isReversed
 		}
 	}
 
@@ -188,7 +200,7 @@ export class CanvasSection extends Control {
 	drawScene() {
 		this.boardMatrix.forEach((row, rI) => {
 			row.forEach((cell, cI) => {
-				this.ctx.fillStyle = cell === 2 ? "red" : cell === 99 ? "green" : 'darkGreen';
+				this.ctx.fillStyle = cell === 5 ? "olive" : cell === 2 ? "red" : cell === 99 ? "green" : 'darkGreen';
 				this.ctx.strokeStyle = 'red';
 				this.ctx.stroke()
 				this.ctx.fillRect((cI * this.cellSize) + 1, (rI * this.cellSize) + 1, this.cellSize - 2, this.cellSize - 2);
@@ -219,6 +231,26 @@ export class CanvasSection extends Control {
 		image.height = height
 		image.onload = () => {
 			callback(image)
+		}
+	}
+
+	private fillShipArea(x: number, y: number, size: number) {
+		for (let i = 0; i < size; i++) {
+			if (!this.isReversed) {
+				this.boardMatrix[y][x + i] = this.boardMatrixBlockedCell
+				this.circleSteps.forEach(stp => {
+					if (!this.boardMatrix[y + stp.y][x + i + stp.x]) return
+					this.boardMatrix[y + stp.y][x + i + stp.x] = this.boardMatrixBlockedCell
+					this.intersectionData.add(`${y + stp.y}-${x + i + stp.x}`)
+				})
+			} else {
+				this.boardMatrix[y + i][x] = this.boardMatrixBlockedCell
+				this.circleSteps.forEach(stp => {
+					if (!this.boardMatrix[y + i + stp.y][x + stp.x])
+						(this.boardMatrix[y + i + stp.y][x + stp.x] = this.boardMatrixBlockedCell)
+					this.intersectionData.add(`${y + i + stp.y}-${x + stp.x}`)
+				})
+			}
 		}
 	}
 }
