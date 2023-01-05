5a8666df2240149e0b56c91340d2c914133515cb
poli8512 <poli8512@gmail.com>
2023-01-05 07:50:53 +0300

    fix ships bug

--- client/src/components/chooseShip/ChooseComponent.tsx
+++ client/src/components/chooseShip/ChooseComponent.tsx
@@ -18,33 +18,41 @@ export enum ShipsSizes {
 }
 
 const ChooseComponent = () => {
-	const [ships, setShips] = useState({
+	const canvasRef = useRef(null)
+	const shipsRef = useRef(null)
+	const ships = useRef({
 		huge: ShipsCount["huge"],
 		large: ShipsCount["large"],
 		medium: ShipsCount["medium"],
 		small: ShipsCount["small"]
 	})
-	const canvasRef = useRef(null)
-	const shipsRef = useRef(null)
 	const [canvSection, setCanvSection] = useState(null)
 	const [activeShip, setActiveShip] = useState<string>(null)
 	useEffect(() => {
 		canvSection?.addActiveShip(activeShip)
+		console.log("ACTIVESHIP", activeShip)
 	}, [activeShip])
-
 	useEffect(() => {
 		const canvas = new CanvasSection(shipsRef.current, (ship) => {
-			setShips({...ships, [ship]: ships[ship as keyof typeof ships] - 1})
-			setActiveShip(null)
+			ships.current = {
+				...ships.current, [ship]: (ships.current[ship as keyof typeof ships.current] - 1)
+			}
+			setActiveShip(activeShip)
 		})
 		setCanvSection(canvas)
 	}, [])
+
 	return (
 		<>
 			<div ref={shipsRef}>
 				<h5>Расставьте корабли</h5>
-				<ShipsSection ships={ships} onAddActiveShip={(type: string) => {
-					setActiveShip(type)
+				<ShipsSection ships={ships.current} onAddActiveShip={(type: string) => {
+					if (type === activeShip) {
+						setActiveShip('')
+						setTimeout(() => setActiveShip(type), 0)
+					} else {
+						setActiveShip(type)
+					}
 				}}/>
 			</div>
 			<canvas ref={canvasRef}/>
