f673928a2ec0a5c3df4cb2175f1d26b983f780e5
unknown <tonyaberch@gmail.com>
2023-01-26 19:18:15 +0100

    fix: merge style, change page

--- client/src/components/application/app.tsx
+++ client/src/components/application/app.tsx
@@ -52,8 +52,8 @@ export const App = () => {
 			setImagesObj(imagesObj)
 			const webSocket = new SocketModel({dispatch});
 			setSocket(webSocket);
-			 //setContent(<Registration socket={webSocket}/>);
-			 setContent(<FinishPage/>)
+			setContent(<Registration socket={webSocket}/>);
+			
 			//setContent(<ChooseShip socket={socket} imagesObj={imagesObj}/>);
 			//setContent(	<GameField socket={socket} shipsImages={imagesObj}></GameField>)
 			return () => {
